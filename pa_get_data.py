cd9bfa4a8c9e6b7c99ad60cbb7f6970a3a749587
wawzat <wawzat@gmail.com>
2020-10-04 17:27:43 -0700

    Cleanup filename template

--- pa_get_data.py
+++ pa_get_data.py
@@ -101,8 +101,9 @@ def get_ts_data(sensor_ids, data_directory, yr, mnth):
       end_time = datetime.strptime(end_time_str, "%Y-%m-%d")
       data_range = list(date_range(start_time, end_time, 4)) 
       #filename = sensor_name + " (" + str(lat) + " " + str(lon) + ")" + " Primary" + " " + str(mnth) + "_" + str(mnth_range[0]) + "_" + str(yr) + " " + str(mnth) + "_" + str(mnth_range[1]) + "_" + str(yr) + ".csv"
-      filename_template = 'sensor_name ({lat} {lon}) Primary {mnth}_{first_day}_{yr} {mnth}_{last_day}_{yr}.csv'
+      filename_template = '{sensor_name} ({lat} {lon}) Primary {mnth}_{first_day}_{yr} {mnth}_{last_day}_{yr}.csv'
       params = {
+         'sensor_name': sensor_name,
          'lat': str(lat),
          'lon': str(lon),
          'mnth': str(mnth),
