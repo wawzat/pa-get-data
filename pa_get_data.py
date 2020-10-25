bedf4180f7299aeb8d80d9397d0974d7789681e5
wawzat <wawzat@gmail.com>
2020-10-25 10:11:28 -0700

    Added UTC offset. Variables now in config.py. Added argparse

--- pa_get_data.py
+++ pa_get_data.py
@@ -1,37 +1,50 @@
-# Get PurpleAir sensor data from PurpleAir and Thingspeak API's for bounding box and year-month
-# James S. Lucas 20201004
-# Todo:  prepare as function accepting input for bbox and date range
-#        convert sensor_ids tuple to dictionary
-#        rename variables consistently with names that are more appropriate to function
-#        allow partial month date range
-#        exception handling
-#        directories and paths from config file vs hardcoded. Prompt for path on first run.
-#        add option to store data in local time vs UTC
+# Get PurpleAir sensor data from PurpleAir and Thingspeak API's for year-month and store as csv files
+# similar to downloaded CSV's from PurpleAir map list.
+# Rename config_template py to config.py and edit to include PurpleAir keys, directory path and bounding box variables.
+# James S. Lucas 20201025
 
 import requests
 import json
 import os
 import config
+import argparse
 import pandas as pd
 import calendar
 from datetime import datetime
+import pytz
 
 
-#user_directory = r' '
-matrix5 = r'd:\Users\James\OneDrive\Documents\House\PurpleAir'
-virtualbox = r'/media/sf_PurpleAir'
-servitor = r'c:\Users\Jim\OneDrive\Documents\House\PurpleAir'
-wsl_ubuntu_matrix5 = r'/mnt/d/Users/James/OneDrive/Documents/House/PurpleAir'
-wsl_ubuntu_servitor = r'/mnt/c/Users/Jim/OneDrive/Documents/House/PurpleAir'
+# Change this variable to point to the desired directory variable in config.py. 
+data_directory = config.matrix5
 
-# Change this variable to point to the desired directory above. 
-data_directory = matrix5
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+    description='Dowload PurpleAir csv files for sensors in bounding box during year and month provided.',
+    prog='pa_get_data',
+    usage='%(prog)s [-y <year>], [-m <month>]',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    g=parser.add_argument_group(title='arguments',
+          description='''    -y, --year   year to get data for.
+    -m  --month  month to get data for.                                        ''')
+    g.add_argument('-y', '--year',
+                    type=int,
+                    dest='yr',
+                    help=argparse.SUPPRESS)
+    g.add_argument('-m', '--month',
+                    type=int,
+                    dest='mnth',
+                    help=argparse.SUPPRESS)
+    args = parser.parse_args()
+    return(args)
+
+args = get_arguments()
 
 
 def get_sensor_indexes():
    root_url = "https://api.purpleair.com/v1/sensors"
-   #           SE lon / lat            NW lon / lat
-   bbox = ['-117.5298', '33.7180', '-117.4166', '33.8188']
+   bbox = config.bbox
    #sensor_index is returned automatically and doesn't need to be included in params fields
    params = {
       'fields': "name,latitude,longitude",
@@ -47,8 +60,6 @@ def get_sensor_indexes():
       header = {"X-API-Key":config.READ_KEY}
       response = requests.get(url, headers=header)
       if response.status_code == 200:
-         #json_response = response.json()
-         #print(json.dumps(json_response, indent=4, sort_keys=True))
          sensors_data = json.loads(response.text)
          for sensor_list in sensors_data['data']:
             list_of_sensor_indexes.append(sensor_list[0])
@@ -90,17 +101,19 @@ def date_range(start, end, intv):
 
 
 def get_ts_data(sensor_ids, data_directory, yr, mnth):
+   tz_offset = int(pytz.timezone('US/Pacific').localize(datetime(yr,mnth,1)).strftime('%z')) * -1 // 100
+   time_offset = '0' + str(tz_offset) + ':00:00'
    for sensor in sensor_ids:
       sensor_name = sensor[0]
       lat = sensor[1]
       lon = sensor[2]
       # returns a tuple (first_day_of_month, last_day_of_month)
       mnth_range = calendar.monthrange(yr, mnth)
-      start_time_str = str(yr) + "-" + str(mnth) + "-" + str(mnth_range[0])
-      start_time = datetime.strptime(start_time_str, "%Y-%m-%d")
-      end_time_str = str(yr) + "-" + str(mnth) + "-" + str(mnth_range[1])
-      end_time = datetime.strptime(end_time_str, "%Y-%m-%d")
-      data_range = list(date_range(start_time, end_time, 4)) 
+      start_date_str = str(yr) + "-" + str(mnth) + "-" + str(mnth_range[0])
+      start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+      end_date_str = str(yr) + "-" + str(mnth+1) + "-" + '1'
+      end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
+      data_range = list(date_range(start_date, end_date, 4)) 
       filename_template = '{sensor_name} ({lat} {lon}) Primary {mnth}_{first_day}_{yr} {mnth}_{last_day}_{yr}.csv'
       params = {
          'sensor_name': sensor_name,
@@ -112,22 +125,23 @@ def get_ts_data(sensor_ids, data_directory, yr, mnth):
          'yr': str(yr)
          }
       filename = filename_template.format(**params)
-      output_folder = start_time.strftime('%Y-%m')
+      output_folder = start_date.strftime('%Y-%m')
       output_path = data_directory + os.path.sep + output_folder
       if not os.path.isdir(output_path):
          os.mkdir(output_path)
       output_pathname = output_path + os.path.sep + filename
       for t in range(0, 4):
-         root_url = 'https://api.thingspeak.com/channels/{channel}/feeds.csv?api_key={api_key}&start={start}%2000:00:00&end={end}%2023:59:59'
+         root_url = 'https://api.thingspeak.com/channels/{channel}/feeds.csv?api_key={api_key}&start={start}%20{offset}&end={end}%20{offset}'
          channel = sensor[4]
          api_key = sensor[5]
-         start_time = data_range[t]
-         end_time = data_range[t+1]
+         start_date = data_range[t]
+         end_date = data_range[t+1]
          params = {
             'channel': channel,
             'api_key': api_key,
-            'start': start_time,
-            'end': end_time
+            'start': start_date,
+            'end': end_date,
+            'offset': time_offset
             }
          url = root_url.format(**params)
          print(url)
@@ -154,8 +168,6 @@ def get_ts_data(sensor_ids, data_directory, yr, mnth):
 
 
 #Main
-yr = 2020
-mnth = 9
 list_of_sensor_indexes = get_sensor_indexes()
 sensor_ids = get_sensor_ids(list_of_sensor_indexes)
-get_ts_data(sensor_ids, data_directory, yr, mnth)
+get_ts_data(sensor_ids, data_directory, args.yr, args.mnth)
