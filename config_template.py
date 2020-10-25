4437ce4c2642cd8ba4ab586747a062124b12b727
wawzat <wawzat@gmail.com>
2020-10-25 10:10:33 -0700

    Initial commit

--- config_template.py
+++ config_template.py
@@ -5,3 +5,6 @@ WRITE_KEY = 'INSERT WRITE KEY KERE'
 
 #user_directory = r' '
 root_directlry = r'd:\Users\UserName\foo\bar'
+
+#           SE lon / lat            NW lon / lat
+bbox = ['-118.6298', '33.9180', '-118.4166', '33.7188'] 
