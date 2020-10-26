a464e11f4465dadddf9ff66f745a5ffac0a5e14a
wawzat <wawzat@gmail.com>
2020-10-25 18:49:41 -0700

    Add variables for get_weather.py

--- config_template.py
+++ config_template.py
@@ -8,3 +8,10 @@ root_directlry = r'd:\Users\UserName\foo\bar'
 
 #           SE lon / lat            NW lon / lat
 bbox = ['-118.6298', '33.9180', '-118.4166', '33.7188'] 
+
+# Darksky Key
+API_KEY = 'INSERT DARKSKY API KEY HERE'
+
+# Change to your home coordinates
+HOME = 34.6425311, -115.362123
+
