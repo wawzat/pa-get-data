b51f8c49a9e81021c836e083006d944d7e49e55f
wawzat <wawzat@gmail.com>
2021-03-18 17:25:38 -0700

    First commit

---
+++ json_test.py
@@ -0,0 +1,28 @@
+# Get PurpleAir sensor data from PurpleAir url into dataframe
+# James S. Lucas 20210318
+
+import pandas as pd
+import json
+
+def get_sensor_data(sensor_id):
+   root_url = "https://www.purpleair.com/json?show="
+
+   params = {
+      'sensor_id': sensor_id
+      }
+   url_template = root_url + "{sensor_id}"
+   url = url_template.format(**params)
+   print(url)
+   try:
+      df = pd.read_json(url)
+      
+   except Exception as e:
+      print(e)
+   return df
+
+sensor_id = 9208
+df = get_sensor_data(sensor_id)
+
+print(df['results'][0]['Stats'])
+stats_dict = json.loads(df['results'][0]['Stats'])
+print(stats_dict['v5'])
