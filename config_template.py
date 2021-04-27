1671fa9f8f7be0c57dea3a94df9574829ec56504
wawzat <wawzat@gmail.com>
2021-04-27 16:31:52 -0700

    Update for new features

--- config_template.py
+++ config_template.py
@@ -1,17 +1,25 @@
 #PurpleAir Keys
-READ_KEY = 'INSERT READ KEY HERE'
-WRITE_KEY = 'INSERT WRITE KEY KERE'
-
+PURPLEAIR_READ_KEY = 'INSERT READ KEY HERE'
+PURPLEAIR_WRITE_KEY = 'INSERT WRITE KEY KERE'
 
 #user_directory = r' '
 root_directlry = r'd:\Users\UserName\foo\bar'
 
 #           SE lon / lat            NW lon / lat
+# Coordinates dictionary for use with regional flag.
+bbox_dict = {
+    'TV': [['-117.5298', '33.7180', '-117.4166', '33.8188'], ''] ,
+    'RS': [['-117.448311', '33.869845', '-117.282829', '34.002297'], 'r'],
+    'OC': [['-117.936172', '33.425138', '-117.566071', '33.782001'], 'o'],
+    'CEP': [['-117.811203', '33.932821', '-117.573280', '34.084228'], 'c']
+    }
+
+#           SE lon / lat            NW lon / lat
 bbox = ['-118.6298', '33.9180', '-118.4166', '33.7188'] 
 
 # Darksky Key
-API_KEY = 'INSERT DARKSKY API KEY HERE'
+DARKSKY_API_KEY = 'INSERT DARKSKY API KEY HERE'
 
 # Change to your home coordinates
-HOME = 34.6425311, -115.362123
+HOME = 35.4425311, -119.332123  
 
